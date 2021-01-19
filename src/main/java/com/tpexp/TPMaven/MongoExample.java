db0acd835ead2cb38c4dce720d4cff25cd2ca193
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-01-20 00:55:46 +0100

    connection au base de donnees Mongodb

--- src/main/java/com/tpexp/TPMaven/MongoExample.java
+++ src/main/java/com/tpexp/TPMaven/MongoExample.java
@@ -1,50 +1,19 @@
 package com.tpexp.TPMaven;
 
-import org.bson.Document;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
 
 public class MongoExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+		try {
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
+			DB database = mongoClient.getDB("myMongoDb");
+			System.out.println("vous êtes connecté");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("le serveur est prêt");
 
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-
-        MongoDatabase database = mongoClient.getDatabase("myMongoDb");
-
-        // print existing databases
-        mongoClient.getDatabaseNames().forEach(System.out::println);
-
-        database.createCollection("customers", null);
-
-        // print all collections in customers database
-        database.getCollectionNames().forEach(System.out::println);
-
-        // create data
-        MongoCollection<Document> collection = database.getCollection("customers");
-        BasicDBObject document = new BasicDBObject();
-        document.put("name", "Shubham");
-        document.put("company", "Baeldung");
-        collection.insert(document);
-
-        // update data
-        BasicDBObject query = new BasicDBObject();
-        query.put("name", "Shubham");
-        BasicDBObject newDocument = new BasicDBObject();
-        newDocument.put("name", "John");
-        BasicDBObject updateObject = new BasicDBObject();
-        updateObject.put("$set", newDocument);
-        collection.update(query, updateObject);
-
-        // read data
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("name", "John");
-        MongoCursor cursor = collection.find(searchQuery);
-        while (cursor.hasNext()) {
-            System.out.println(cursor.next());
-        }
     }
 }
