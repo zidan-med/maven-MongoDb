ee5d57dd5da99642a3eec788dbf07bd5858f80e8
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-01-20 11:05:23 +0100

    modifications des donnees

--- src/main/java/com/tpexp/TPMaven/MongoExample.java
+++ src/main/java/com/tpexp/TPMaven/MongoExample.java
@@ -9,17 +9,29 @@ import com.mongodb.MongoClient;
 public class MongoExample {
     public static void main(String[] args) throws Exception{
 		try {
+			//connection au base de donnees Mongodb
 			MongoClient mongoClient = new MongoClient("localhost", 27017);
 			DB database = mongoClient.getDB("myMongoDb");
 			System.out.println("vous êtes connecté");
+			//afficher les base de donnees
 			mongoClient.getDatabaseNames().forEach(System.out::println);
 			database.createCollection("customers", null);
+			//afficher les collections
 			database.getCollectionNames().forEach(System.out::println);
+			//insertion des donnees
 			DBCollection collection = database.getCollection("customers");
 	        BasicDBObject document = new BasicDBObject();
 	        document.put("name", "Shubham");
 	        document.put("company", "Baeldung");
 	        collection.insert(document);
+	        //modification des donnees
+	        BasicDBObject query = new BasicDBObject();
+	        query.put("name", "Shubham");
+	        BasicDBObject newDocument = new BasicDBObject();
+	        newDocument.put("name", "John");
+	        BasicDBObject updateObject = new BasicDBObject();
+	        updateObject.put("$set", newDocument);
+	        collection.update(query, updateObject);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
