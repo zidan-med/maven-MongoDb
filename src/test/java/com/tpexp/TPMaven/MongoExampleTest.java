3c4adf542b35847233142d8678f3da84b0b8ec72
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-01-21 13:03:29 +0100

    ajouter les dependences Junit

---
+++ src/test/java/com/tpexp/TPMaven/MongoExampleTest.java
@@ -0,0 +1,14 @@
+package com.tpexp.TPMaven;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MongoExampleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
