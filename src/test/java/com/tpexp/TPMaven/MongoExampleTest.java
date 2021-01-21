981c2063ac4ee3a42dfa8b1b0932bd1be4cf0b7c
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-01-21 21:35:57 +0100

    ceation du teste

--- src/test/java/com/tpexp/TPMaven/MongoExampleTest.java
+++ src/test/java/com/tpexp/TPMaven/MongoExampleTest.java
@@ -2,13 +2,19 @@ package com.tpexp.TPMaven;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.UnknownHostException;
+
 import org.junit.jupiter.api.Test;
+
+import com.mongodb.MongoClient;
 
 class MongoExampleTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws UnknownHostException {
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		mongoClient.getDatabaseNames().forEach(System.out::println);
+		
 	}
 
 }
